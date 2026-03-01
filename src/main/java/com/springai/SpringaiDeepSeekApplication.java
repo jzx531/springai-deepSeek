d12ad2c577e4d206d60c56e6927bea247c53d873
jzx531 <115254510+jzx531@users.noreply.github.com>
2026-03-01 18:03:18 +0800

    Initial commit

---
+++ src/main/java/com/springai/SpringaiDeepSeekApplication.java
@@ -0,0 +1,13 @@
+package com.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiDeepSeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaiDeepSeekApplication.class, args);
+    }
+
+}
